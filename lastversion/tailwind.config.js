cc7ac42a0b265a10a3bfe3d22f7f06d4474dcf5b
scsancho <sandracatchot@gmail.com>
2024-11-22 14:31:24 +0100

    22112024

---
+++ lastversion/tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    colors: {
+      'azulS': '#124559',
+      'blancoS': '#E8EBE4',
+      'amarilloS': '#ffa630',
+      'oscuroS': '#01161e',
+    },
+
+    fontFamily: {
+      'primary-font': ['Lato', 'sans-serif'],
+      'secondary-font': ['Merriweather Sans', 'sans-serif'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
